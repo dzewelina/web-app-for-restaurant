d952139c0d84067cd62cac1e5a35b60909157327
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-25 15:56:49 +0000

    Fix res status for addBooking

--- controllers/addBooking.js
+++ controllers/addBooking.js
@@ -3,7 +3,7 @@ const { createNewBooking } = require('../models/bookings');
 const addBooking = (req, res, next) => {
   const data = req.body;
   createNewBooking(data)
-    .then(newBooking => res.send(newBooking))
+    .then(newBooking => res.status(201).send({ newBooking }))
     .catch(next);
 };
 
